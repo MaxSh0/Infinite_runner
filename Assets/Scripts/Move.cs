d7485b05b12c9c9d5b84edb58370b8e0155945b8
Maxim Shakurov <m.shakurov99@mail.ru>
2020-11-09 22:09:06 +0400

    Lab4

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, -0.1f));
+        //для тестов
+        if (transform.position.z <= -8)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public GameObject[] Platforms;
+    public GameObject[] prefabPlatforms;
+
+    void Update()
+    {
+
+        if(Platforms[0] == null)
+        {
+            Debug.Log("Object is invisible");
+            Platforms[0] = Instantiate(prefabPlatforms[Random.Range(0, 5)], new Vector3(0, 0, 41), Quaternion.identity);
+        }
+        else if (Platforms[1] == null)
+        {
+            Debug.Log("Object is invisible");
+            Platforms[1] = Instantiate(prefabPlatforms[Random.Range(0, 5)], new Vector3(0, 0, 41), Quaternion.identity);
+        }
+        else if (Platforms[2] == null)
+        {
+            Debug.Log("Object is invisible");
+            Platforms[2] = Instantiate(prefabPlatforms[Random.Range(0, 5)], new Vector3(0, 0, 41), Quaternion.identity);
+        }
+        else if (Platforms[3] == null)
+        {
+            Debug.Log("Object is invisible");
+            Platforms[3] = Instantiate(prefabPlatforms[Random.Range(0, 5)], new Vector3(0, 0, 41), Quaternion.identity);
+        }
+        else if (Platforms[4] == null)
+        {
+            Debug.Log("Object is invisible");
+            Platforms[4] = Instantiate(prefabPlatforms[Random.Range(0, 5)], new Vector3(0, 0, 41), Quaternion.identity);
+        }
+
+
+    }
+
+
+}
